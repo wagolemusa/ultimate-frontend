3d1406d8a6a0b7dcd51e60513094fd2b3330294e
wagolemusa <homiemusa@gmail.com>
2022-11-02 09:58:10 +0300

    changes

--- src/admin/GetUsers.js
+++ src/admin/GetUsers.js
@@ -8,7 +8,7 @@ const GetUsers = () =>{
   const [userinfo, setUserinfo ] = useState()
 
   const getUser = () =>{
-    axios.get('http://localhost:5000/users/api/authenticate', {
+    axios.get('https://ultimatebackend.herokuapp.com/users/api/authenticate', {
 
       headers: {
         'Authorization': token,
